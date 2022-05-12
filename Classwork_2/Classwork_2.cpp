0e1715758ae1e11fd43fbc047b4a66e54bf128b0
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-12 13:18:51 +0300

    Добавьте файлы проекта.

---
+++ Classwork_2/Classwork_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+   /* if (a > b && a > c && a > d && a > d && a > e) cout << a;
+    else if (b > c && b > d && b > e) cout << b;
+    else if (c > d && c > e) cout << c;
+    else if (d > e) cout << d;
+    else cout << e;*/
+
+    cout << max(max(max(max(a, b), c), d), e);
+}
